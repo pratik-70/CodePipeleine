d4786c86fa9e87ce7efae7d6e656e8c893118781
pratik708 <70812345pratik@gmail.com>
2026-06-18 00:20:49 +0530

    add changes

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -2,6 +2,7 @@
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -10,7 +11,14 @@ public class LoginTest {
     @Test
     public void googleTest() {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
 
